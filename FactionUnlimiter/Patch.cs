be0661d696da7355b27cfcba50a2a2e4ba340cdc
zznty <94796179+zznty@users.noreply.github.com>
2022-11-13 00:23:17 +0700

    first

---
+++ FactionUnlimiter/Patch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Sandbox.Game.Gui;
+using SpaceEngineers.Game.GUI;
+
+namespace FactionUnlimiter
+{
+    [HarmonyPatch(typeof(MyGuiScreenCreateOrEditFactionSpace), nameof(MyGuiScreenCreateOrEditFactionSpace.RecreateControls))]
+    public static class Patch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldc_I4_3)
+                {
+                    instruction.opcode = OpCodes.Ldc_I4;
+                    instruction.operand = 512;
+                }
+                
+                yield return instruction;
+            }
+        }
+    }
+    
+    [HarmonyPatch(typeof(MyGuiScreenCreateOrEditFaction), "OnOkClick")]
+    public static class Patch2
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var ins = instructions.ToList();
+            for (var i = 0; i < ins.Count; i++)
+            {
+                var instruction = ins[i];
+                
+                if (instruction.opcode == OpCodes.Ldc_I4_3)
+                {
+                    instruction.opcode = OpCodes.Ldc_I4;
+                    instruction.operand = 512;
+                    ins[i + 1].opcode = OpCodes.Ble_S;
+                }
+
+                yield return instruction;
+            }
+        }
+    }
+}
---
+++ FactionUnlimiter/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using VRage.Plugins;
+
+namespace FactionUnlimiter
+{
+    public class Plugin : IPlugin
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Init(object gameInstance)
+        {
+            new Harmony("FactionUnlimiter").PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
